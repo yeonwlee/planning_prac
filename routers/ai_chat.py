66e2a4fbe26ffcba5587223b87d685ad1aef175d
yeonwlee <tlstprl1806@naver.com>
2024-09-10 18:00:29 +0900

    chore: 줄바꿈

--- routers/ai_chat.py
+++ routers/ai_chat.py
@@ -10,7 +10,14 @@ from schemas.ai_chat import Proposal
 import pprint
 
 chat_router = APIRouter()
-proposal_chatbot = CustomChatBot(kind_of_model='gpt-4o', model_name='proposal', prompt_concept='기획서 작성', detail_prompt='해당 내용에 적합하게 기획서를 작성해줘', temperature=0.1, history=True)
+proposal_chatbot = CustomChatBot(
+                                 kind_of_model='gpt-4o',
+                                 model_name='proposal', 
+                                 prompt_concept='기획서 작성', 
+                                 detail_prompt='해당 내용에 적합하게 기획서를 작성해줘', 
+                                 temperature=0.1, 
+                                 history=True
+                                 )
 
 @chat_router.post('/proposal')
 async def proposal(data:Proposal) -> dict:
